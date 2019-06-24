200c72d2556fc54f26247ee1ca659bf240c50f84
Katana Tran <tkatyk21@gmail.com>
2019-06-24 06:49:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -26,6 +26,6 @@ def assign_rooms(array)
 end 
   
 def printer(attendees)
-  batch_badge_creator(attendees)
-  assign_rooms(attendees)
+  puts batch_badge_creator(attendees)
+  puts assign_rooms(attendees)
 end  
